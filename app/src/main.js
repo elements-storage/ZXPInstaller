2df9e3fd52097d635084bb58723a703c29ca69ce
Cameron McEfee <cameron@github.com>
2015-10-26 08:31:19 -0700

    Just add all the messages

--- app/src/main.js
+++ app/src/main.js
@@ -29,8 +29,16 @@ global.View = function() {
 
   var installationFailed = function(error) {
     var errors = {
-      402: 'Installation failed because the extension\'s signature is invalid.'
-      456: 'Please close all Adobe applications and try again.'
+      175: 'Installation failed because you do not have administrator access.',
+      201: 'Installation failed because the extension invalid.',
+      411: 'Installation failed because the extension is not compatible with the installed applications.',
+      407: 'Installation failed because this extension requires another extension.',
+      408: 'Installation failed because this extension requires another extension.',
+      412: 'Installation failed because an extension of the same name exists.',
+      418: 'Installation failed because a newer version of the extension is installed.',
+      456: 'Installation failed because Adobe applications are running',
+      458: 'Installation failed because none of the required applications are installed',
+      459: 'Installation failed because the extension is not compatible with the installed applications.'
     };
 
     view.removeChild(spinner.el);
