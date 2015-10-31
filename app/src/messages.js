1daba0f66e301664107513fe41fd6a8587d9cffa
Cameron McEfee <cameron@github.com>
2015-10-31 10:54:11 -0700

    More actionable messages

--- app/src/messages.js
+++ app/src/messages.js
@@ -1,14 +1,14 @@
 global.Messages = function() {
 
   this.errors = {
-    175: 'Installation failed because you do not have administrator access.',
+    175: 'You must run ZXP Installer in administrator mode to install extensions.',
     201: 'Installation failed because the extension invalid.',
     411: 'Installation failed because the extension is not compatible with the installed applications.',
     407: 'Installation failed because this extension requires another extension.',
     408: 'Installation failed because this extension requires another extension.',
     412: 'Installation failed because an extension of the same name exists.',
     418: 'Installation failed because a newer version of the extension is installed.',
-    456: 'Installation failed because Adobe applications are running',
+    456: 'Please close all Adobe applications before installing extensions.',
     458: 'Installation failed because none of the required applications are installed',
     459: 'Installation failed because the extension is not compatible with the installed applications.'
   };
